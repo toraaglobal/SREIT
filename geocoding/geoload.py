c369290870f423e529e1ee3f803c1e04b97eaa0c
Tajudeen Abdulazeez <teejay4show@yahoo.com>
2019-05-10 00:04:01 -0700

    geocoding used for metro

---
+++ geocoding/geoload.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  9 16:52:30 2018
+
+@author: teejay
+"""
+
+import urllib.request, urllib.parse, urllib.error
+import http
+import sqlite3
+import json
+import time
+import ssl
+import sys
+import os
+
+
+
+api_key = os.environ.get('GOOGLE_API')
+
+if api_key is None:
+    api_key =input('Enter your google API key:-')
+    
+    
+serviceurl = "https://maps.googleapis.com/maps/api/place/textsearch/json?"
+
+# create a database connection
+conn = sqlite3.connect('geodata.sqlite')
+cur = conn.cursor()
+
+cur.execute('''
+CREATE TABLE IF NOT EXISTS Locations (address TEXT, geodata TEXT)''')
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+fh = open("metro.data")  # where all datas to search for geolocation are saved
+count = 0
+for line in fh:
+    if count > 200 :
+        print('Retrieved 200 locations, restart to retrieve more')
+        break
+
+    address = line.strip()
+    print('')
+    cur.execute("SELECT geodata FROM Locations WHERE address= ?",
+        (memoryview(address.encode()), ))
+
+    try:
+        data = cur.fetchone()[0]
+        print("Found in database ",address)
+        continue
+    except:
+        pass
+
+    parms = dict()
+    parms["query"] = address
+    parms['key'] = api_key
+    url = serviceurl + urllib.parse.urlencode(parms)
+
+    print('Retrieving', url)
+    uh = urllib.request.urlopen(url, context=ctx)
+    data = uh.read().decode()
+    print('Retrieved', len(data), 'characters', data[:20].replace('\n', ' '))
+    count = count + 1
+
+    try:
+        js = json.loads(data)
+    except:
+        print(data)  # We print in case unicode causes an error
+        continue
+
+    if 'status' not in js or (js['status'] != 'OK' and js['status'] != 'ZERO_RESULTS') :
+        print('==== Failure To Retrieve ====')
+        print(data)
+        break
+
+    cur.execute('''INSERT INTO Locations (address, geodata)
+            VALUES ( ?, ? )''', (memoryview(address.encode()), memoryview(data.encode()) ) )
+    conn.commit()
+    if count % 10 == 0 :
+        print('Pausing for a bit...')
+        time.sleep(5)
+
+print("Run geodump.py to read the data from the database so you can vizualize it on a map.")
